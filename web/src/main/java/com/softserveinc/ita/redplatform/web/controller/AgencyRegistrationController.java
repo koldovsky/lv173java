b00c592e05aa15de0fc29afef4d70f3fb4ca84c6
Oleh Khimka <olkhima@gmail.com>
2016-01-31 18:33:33 +0200

    code refactoring

---
+++ web/src/main/java/com/softserveinc/ita/redplatform/web/controller/AgencyRegistrationController.java
@@ -0,0 +1,37 @@
+package com.softserveinc.ita.redplatform.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Controller for registration new Real Estate Agency.
+ * 
+ * @author Oleh Khimka
+ *         
+ */
+@Controller
+public class AgencyRegistrationController {
+    
+    /**
+     * registerAgency page mapping.
+     * 
+     * @return agencyRegistration view.
+     */
+    @RequestMapping(value = "registerAgency", method = RequestMethod.GET)
+    public final String getPage() {
+	return "agencyRegistration";
+    }
+    
+    /**
+     * register Agency.
+     * 
+     * @return index view.
+     */
+    @RequestMapping(value = "registerAgency", method = RequestMethod.POST)
+    public final String registerAgency() {
+	System.out.println("in register Agency controller");
+	return "index";
+    }
+
+}
