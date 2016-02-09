ac461263bcdec89fc7d7e64bf387714dbc06ae8b
bulhakovolexiy <dzot161995@gmail.com>
2016-02-09 14:16:16 +0200

    Secure Random created as Spring Singleton.

--- business/src/main/java/com/softserveinc/ita/redplatform/business/accessory/SimplePassGeneratorImpl.java
+++ business/src/main/java/com/softserveinc/ita/redplatform/business/accessory/SimplePassGeneratorImpl.java
@@ -1,6 +1,9 @@
 package com.softserveinc.ita.redplatform.business.accessory;
 
+import java.security.SecureRandom;
+
 import org.apache.commons.lang3.RandomStringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 /**
  * Implementation of password generation method.
  * @author Bulhakov Alex
@@ -8,11 +11,17 @@ import org.apache.commons.lang3.RandomStringUtils;
  */
 public class SimplePassGeneratorImpl implements PasswordGenerator {
 
+    /**
+     * Secure Random instance created as Spring singleton.
+     */
+    @Autowired
+    private SecureRandom secureRandom;
     
     @Override
     public final String generatePassword(final int length) {
-	
-	return RandomStringUtils.random(length, true, true);
+
+	return RandomStringUtils.random(length, 0, 0, true,
+		true, null, secureRandom);
     }
 
 }
