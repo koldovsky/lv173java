1e9d9c3193c6cd7224690e24842a67f8844f5944
Hryhorii Somyk <somykhryhorii@gmail.com>
2016-02-11 02:57:35 +0200

    modified method findUserByEmail in class JPAUserDAO

--- persistence/src/main/java/com/softserveinc/ita/redplatform/persistence/dao/impl/JPAUserDao.java
+++ persistence/src/main/java/com/softserveinc/ita/redplatform/persistence/dao/impl/JPAUserDao.java
@@ -2,7 +2,6 @@ package com.softserveinc.ita.redplatform.persistence.dao.impl;
 
 import java.util.List;
 
-import javax.persistence.NoResultException;
 import org.springframework.stereotype.Repository;
 
 import com.softserveinc.ita.redplatform.common.entity.RealEstateAdminUser;
@@ -12,40 +11,37 @@ import com.softserveinc.ita.redplatform.persistence.dao.UserDao;
 /**
  * DAO for User.
  * 
- * @author oleh
+ * @author Hryhorii Somyk
  *
  */
 @Repository
 public class JPAUserDao extends JPAGenericDao<User, Long> implements UserDao {
 
-    /**
-     * method find user by email.
-     */
-    @Override
-    public final User findUserByEmail(final String email) {
-	try {
-	    return (User) super.getEntityManager()
-		    .createNamedQuery("FROM User WHERE email=:email")
-		    .setParameter("email", email).getSingleResult();
-	} catch (NoResultException e) {
-	    return null;
+	/**
+	 * method find user by email.
+	 */
+	@Override
+	public final User findUserByEmail(final String email) {
+		return (User) super.getEntityManager()
+				.createQuery("from " + User.class.getName() 
+						+ " as user where user.email=:email")
+				.setParameter("email", email)
+				.getSingleResult();
 	}
-    }
 
-    /**
-     * method find all users dealing with company.
-     * 
-     * @param companyName
-     *            company name
-     * @return List<User>
-     */
-    @SuppressWarnings("unchecked")
-    @Override
-    public final List<User> findUsersByCompany(final String companyName) {
-	return (List<User>) super.getEntityManager()
-		.createQuery("from " + RealEstateAdminUser.class.getName() 
-			+ " as redadmin inner join redadmin.agency "
-			+ "as agency where agency.name=:companyName")
-		.setParameter("companyName", companyName).getResultList();
-    }
+	/**
+	 * method find all users dealing with company.
+	 * 
+	 * @param companyName company name
+	 * @return List<User>
+	 */
+	@SuppressWarnings("unchecked")
+	@Override
+	public final List<User> findUsersByCompany(final String companyName) {
+		return (List<User>) super.getEntityManager()
+				.createQuery("from " + RealEstateAdminUser.class.getName()
+						+ " as redadmin inner join redadmin.agency "
+						+ "as agency where agency.name=:companyName")
+				.setParameter("companyName", companyName).getResultList();
+	}
 }
