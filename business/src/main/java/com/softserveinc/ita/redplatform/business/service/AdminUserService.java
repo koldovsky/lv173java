c25c56edbd4762b3e21d104e0aca19122ad6d998
bulhakovolexiy <dzot161995@gmail.com>
2016-02-12 07:32:38 +0200

    Upgrading the AdminUserService.

--- business/src/main/java/com/softserveinc/ita/redplatform/business/service/AdminUserService.java
+++ business/src/main/java/com/softserveinc/ita/redplatform/business/service/AdminUserService.java
@@ -2,6 +2,8 @@ package com.softserveinc.ita.redplatform.business.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -12,7 +14,6 @@ import com.softserveinc.ita.redplatform.common.mapper.AdminUserMapper;
 import com.softserveinc.ita.redplatform.integration.MailService;
 import com.softserveinc.ita.redplatform.persistence.dao.AdminUserDao;
 
-
 /**
  * Service for AdminUser.
  *
@@ -20,40 +21,56 @@ import com.softserveinc.ita.redplatform.persistence.dao.AdminUserDao;
  */
 @Service
 public class AdminUserService {
-    
+
     /** The admin user dao. */
     @Autowired
     private AdminUserDao adminUserDao;
-    
+
     /** The admin user mapper. */
     @Autowired
     private AdminUserMapper adminUserMapper;
-    
+
     /** The mail service. */
     @Autowired
     private MailService mailService;
-    
+
     /** The password generator. */
     @Autowired
     private PasswordGenerator passwordGenerator;
-    
+
+    /** The password encoder. */
+    private PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
     /**
      * Creates the.
      *
-     * @param adminUserDTO the admin user dto
+     * @param adminUserDTO
+     *            the admin user dto
      */
     @Transactional
     public void create(final AdminUserDTO adminUserDTO) {
 	AdminUser adminUser = adminUserMapper.toEntity(adminUserDTO);
-	adminUser.setPassword(passwordGenerator.generatePassword());
-	SimpleMailMessage simpleMessage = new SimpleMailMessage();
-	simpleMessage.setFrom("Me");
-	simpleMessage.setTo(adminUser.getEmail());
-	simpleMessage.setText(adminUser.getPassword());
-	mailService.sendMessage(simpleMessage);
+	String password = passwordGenerator.generatePassword();
 	
+	notifyUserWithEmail(adminUser.getEmail(), password);
+	adminUser.setPassword(passwordEncoder.encode(password));
 	
+	adminUserDao.save(adminUser);
+
     }
-    
+
+    /**
+     * Method for notifying user by email with his pass. 
+     * @param recipient recipient of the message
+     * @param password generated password
+     */
+    private void notifyUserWithEmail(final String recipient, 
+	    final String password) {
+	SimpleMailMessage simpleMessage = new SimpleMailMessage();
+	simpleMessage.setFrom("REDplatform");
+	simpleMessage.setTo(recipient);
+	simpleMessage.setText(password);
+	mailService.sendMessage(simpleMessage);
+    }
 
 }
