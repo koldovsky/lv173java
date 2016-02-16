dd31859984101e89e3290c9077eacd2a7f6da599
Roman Ivaniv <ivaniv.roma.15@gmail.com>
2016-02-16 02:40:43 +0200

    cleaning checkstyle errors

--- business/src/main/java/com/softserveinc/ita/redplatform/business/service/UserDetailsServiceImplementation.java
+++ business/src/main/java/com/softserveinc/ita/redplatform/business/service/UserDetailsServiceImplementation.java
@@ -5,22 +5,17 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-import com.softserveinc.ita.redplatform.persistence.dao.AdminUserDao;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.softserveinc.ita.redplatform.common.entity.AdminUser;
-import com.softserveinc.ita.redplatform.common.entity.CustomerUser;
 import com.softserveinc.ita.redplatform.common.entity.RealEstateAdminUser;
-import com.softserveinc.ita.redplatform.persistence.dao.impl.JPAAdminUserDao;
 
 /**
  * Implementation of UserDetailsService.
