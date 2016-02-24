dc5a9dbaf853e63b60c34e0055cbf91ca945d142
bulhakovolexiy <dzot161995@gmail.com>
2016-02-24 21:35:24 +0200

    Base classes for ordering implemented.

---
+++ persistence/src/main/java/com/softserveinc/ita/redplatform/persistence/dao/OrderDao.java
@@ -0,0 +1,12 @@
+package com.softserveinc.ita.redplatform.persistence.dao;
+
+import com.softserveinc.ita.redplatform.common.entity.Order;
+
+/**
+ * Dao interface for Order entity.
+ * @author Bulhakov Alex
+ *
+ */
+public interface OrderDao extends GenericDao<Order, Long> {
+    
+}
