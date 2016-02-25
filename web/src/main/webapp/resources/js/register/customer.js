3f45fcaa91d18b6e99b8c5c29bbafc498ab2f985
Hryhorii Somyk <somykhryhorii@gmail.com>
2016-02-25 04:08:30 +0200

    created regex methods for passports and individual tax numbers, added validation to all fields on customer registration page

--- web/src/main/webapp/resources/js/register/customer.js
+++ web/src/main/webapp/resources/js/register/customer.js
@@ -10,6 +10,18 @@ $(document).ready(function() {
 	$('#customer-form').validate({
 		errorClass : 'text-danger',
 		rules : {
+			firstName : {
+				required : true,
+				minlength : 1,
+				maxlength :100,
+				nameCustom :true
+			},
+			lastName : {
+				required : true,
+				minlength : 1,
+				maxlength : 100,
+				nameCustom : true
+			},
 			mail : {
 				required : true,
 				minlength : 6,
@@ -19,7 +31,52 @@ $(document).ready(function() {
 					url : 'checkmail',
 					type : 'GET'
 				}
+			},
+			phone : {
+				required :true,
+				minlength : 10,
+				maxlength : 12,
+				regexPhoneNumber : true
+			},
+			passport : {
+				required :true,
+				minlength : 8,
+				maxlength : 9,
+				regexPassport : true
+			},
+			taxNumber : {
+				required :true,
+				minlength : 9,
+				maxlength : 10,
+				regexIndividualTaxNumber : true
+			},
+			country : {
+				required : true,
+				regexCountry : true,
+				minlength : 2
+			},
+			region : {
+				regexRegion : true,
+				minlength : 2
+			},
+			postalCode : {
+				required : true,
+				regexPostalCode : true,
+				minlength : 3,
+				maxlength : 10
+
+			},
+			locality : {
+				required : true,
+				regexCountry : true,
+				minlength : 2
+			},
+			address : {
+				required : true,
+				minlength : 2,
+				regexAdditional : true
 			}
+			
 		},
 		messages : {
 			mail : {
