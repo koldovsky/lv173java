45d59b9d7796a059446f789ee92dd09b50e4fb52
Hryhorii Somyk <somykhryhorii@gmail.com>
2016-02-29 23:43:23 +0200

    created possibility to add new customers, removed validators from regexes.js, separated validation and creation of new customer in two js files

--- web/src/main/webapp/WEB-INF/views/register/customer.jsp
+++ web/src/main/webapp/WEB-INF/views/register/customer.jsp
@@ -7,6 +7,7 @@
 <title>Customer registration</title>
 <script src="${pageContext.request.contextPath}/resources/js/lib/jquery/jquery.validate.min.js"></script>
 <script src="${pageContext.request.contextPath}/resources/js/register/customer.js"></script>
+<script src="${pageContext.request.contextPath}/resources/js/customer.js"></script>
 <script src="${pageContext.request.contextPath}/resources/js/regexes.js"></script>
 </head>
 <body>
--- web/src/main/webapp/resources/js/register/customer.js
+++ web/src/main/webapp/resources/js/register/customer.js
@@ -1,119 +1,30 @@
 $(document).ready(function() {
 	
-	$('.alert .close').on('click', function(e) {
-	    $(this).parent().hide();
-	});
-
-	$('#success').hide();
-	$('#error').hide();
-
-	$('#customer-form').validate({
-		errorClass : 'text-danger',
-		rules : {
-			firstName : {
-				required : true,
-				minlength : 1,
-				maxlength :100,
-				nameCustom :true
-			},
-			lastName : {
-				required : true,
-				minlength : 1,
-				maxlength : 100,
-				nameCustom : true
-			},
-			mail : {
-				required : true,
-				minlength : 6,
-				maxlength : 60,
-				regexMail: true,
-				remote : {
-					url : 'checkmail',
-					type : 'GET'
-				}
-			},
-			phone : {
-				required :true,
-				minlength : 10,
-				maxlength : 12,
-				regexPhoneNumber : true
-			},
-			passport : {
-				required :true,
-				minlength : 8,
-				maxlength : 9,
-				regexPassport : true
-			},
-			taxNumber : {
-				required :true,
-				minlength : 9,
-				maxlength : 10,
-				regexIndividualTaxNumber : true
-			},
-			country : {
-				required : true,
-				regexCountry : true,
-				minlength : 2
-			},
-			region : {
-				regexRegion : true,
-				minlength : 2
-			},
-			postalCode : {
-				required : true,
-				regexPostalCode : true,
-				minlength : 3,
-				maxlength : 10
-
-			},
-			locality : {
-				required : true,
-				regexCountry : true,
-				minlength : 2
-			},
-			address : {
-				required : true,
-				minlength : 2,
-				regexAdditional : true
-			}
-			
-		},
-		messages : {
-			mail : {
-				required : 'Please enter email',
-				minlength : 'Email must be minimum 6 characters long',
-				maxlength : 'Email must be maximum 60 characters long',
-				regexMail : 'Invalid email',
-				remote : 'This email is already registered'
-			}
-		} 
-	});
-	
-	function createCustomer(){
+	function createCustomer() {
 		var address = {
-				country : $('#country').val(),
-				region : $('#region').val(),
-				postalCode : $('#postalCode').val(),
-				locality : $('#locality').val(),
-				additional : $('#address').val()
+			country : $('#country').val(),
+			region : $('#region').val(),
+			postalCode : $('#postalCode').val(),
+			locality : $('#locality').val(),
+			additional : $('#address').val()
 		}
-		
+
 		var customer = {
-				email : $('#mail').val(),
-				firstName : $('#firstName').val(),
-				lastName : $('#lastName').val(),
-				phone : $('#phone').val(),
-				passport : $('#passport').val(),
-				individualTaxNumber : $('#taxNumber').val(),
-				address : address
+			email : $('#mail').val(),
+			firstName : $('#firstName').val(),
+			lastName : $('#lastName').val(),
+			phone : $('#phone').val(),
+			passport : $('#passport').val(),
+			individualTaxNumber : $('#taxNumber').val(),
+			address : address
 		}
-		
+
 		$.ajax({
 			type : 'POST',
 			url : 'api/customer',
 			contentType : 'application/json; charset=UTF-8',
 			data : JSON.stringify(customer),
-			success: function(responseData, textStatus, jqXHR) {
+			success : function(responseData, textStatus, jqXHR) {
 				$('#customer-form')[0].reset();
 				$('#success').show();
 			},
@@ -122,11 +33,12 @@ $(document).ready(function() {
 			}
 		});
 	}
-	
+
 	$('#customer-form').submit(function(event) {
 		if ($('#customer-form').valid()) {
 			createCustomer();
+			return false;
 		}
 	});
-	
+
 });
