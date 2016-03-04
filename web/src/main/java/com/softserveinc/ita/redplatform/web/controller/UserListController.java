d3e7c234778cd5852014ae3cfa4463e74996ef75
Hryhorii Somyk <somykhryhorii@gmail.com>
2016-03-04 03:33:34 +0200

    corrected security removed filtering from controllers secured pages(users ,customers**) secured methods in customerUserService(update, register)

--- web/src/main/java/com/softserveinc/ita/redplatform/web/controller/UserListController.java
+++ web/src/main/java/com/softserveinc/ita/redplatform/web/controller/UserListController.java
@@ -1,18 +1,10 @@
 package com.softserveinc.ita.redplatform.web.controller;
 
-import java.util.Collection;
-
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.AnonymousAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
 import com.softserveinc.ita.redplatform.business.service.UserService;
 
 /**
@@ -41,27 +33,9 @@ public class UserListController {
 	 * 
 	 * @return users page
 	 */
-	@SuppressWarnings("unchecked")
 	@RequestMapping(value = "/users", method = RequestMethod.GET)
 	public final String getUsersPage() {
-		Authentication auth = SecurityContextHolder
-				.getContext()
-				.getAuthentication();
-		if (auth instanceof AnonymousAuthenticationToken) {
-			return "common/login";
-		} else {
-			Collection<GrantedAuthority> authorities = 
-					(Collection<GrantedAuthority>) SecurityContextHolder
-					.getContext()
-					.getAuthentication()
-					.getAuthorities();
-			if (authorities.contains(new SimpleGrantedAuthority("ROLE_ADMIN")) 
-					|| authorities.contains(
-							new SimpleGrantedAuthority("ROLE_REDADMIN"))) {
-				return "users";
-			}
-		}
-		return "common/index";
+	    return "users";
 	}
 
 }
